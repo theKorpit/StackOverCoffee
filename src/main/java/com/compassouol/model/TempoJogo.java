832110e15d752175c4afec3986693adfa2de8f31
Vinicius <vininiciush@gmail.com>
2020-12-14 17:27:41 -0300

    Criação da classe Agenda e TempoJogo

--- src/main/java/com/compassouol/model/TempoJogo.java
+++ src/main/java/com/compassouol/model/TempoJogo.java
@@ -1,5 +1,45 @@
 package com.compassouol.model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class TempoJogo {
 
+	private LocalDateTime dataInicio;
+	
+	private LocalDateTime dataFim;
+	
+	public TempoJogo(LocalDateTime dataInicio, LocalDateTime dataFim) {
+		if(dataInicio.isAfter(dataFim))
+			throw new RuntimeException("A data de inicio não pode ser depois da data final");
+		
+		this.dataInicio = dataInicio;
+		this.dataFim = dataFim;
+	}
+
+	public LocalDateTime getDataInicio() {
+		return dataInicio;
+	}
+
+	public LocalDateTime getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDateTime dataFim) {
+		this.dataFim = dataFim;
+	}
+	
+	public Float totalTempoJogado() {
+		
+		Duration dur = Duration.between(this.dataInicio, this.dataFim);
+		
+		Float horas = (float) dur.toMinutes()/60;
+		
+		return horas;
+	}
+
+	@Override
+	public String toString() {
+		return "TempoJogo [dataInicio=" + dataInicio + ", dataFim=" + dataFim + "]";
+	}
 }
