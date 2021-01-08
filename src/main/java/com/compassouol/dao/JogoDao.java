cfb33b254522d46779a066930fdc36bd8fa670fa
Joao <joaoarent97@gmail.com>
2021-01-08 16:52:21 -0300

    Apagando os updates e continua funcionando

--- src/main/java/com/compassouol/dao/JogoDao.java
+++ src/main/java/com/compassouol/dao/JogoDao.java
@@ -7,7 +7,7 @@ public class JogoDao {
 	public void save(Jogo jogo) {
 		for (Jogo jogoSearch : BibliotecaDao.biblioteca.getJogos()) {
 			if (jogoSearch.getAppIdSteam() == jogo.getAppIdSteam()) {
-				this.update(jogo);
+				
 				return;
 			}
 		}
@@ -29,14 +29,5 @@ public class JogoDao {
 		}
 		return null;
 	}
-	public void update(Jogo jogo) {
-		int index=32;
-        for(Jogo jogoSearch : BibliotecaDao.biblioteca.getJogos()){
-            if(jogoSearch.getAppIdSteam() == jogo.getAppIdSteam()) {
-            	index = BibliotecaDao.biblioteca.getJogos().indexOf(jogoSearch);
-            }
-                
-            
-        }
-    }
+
 }
