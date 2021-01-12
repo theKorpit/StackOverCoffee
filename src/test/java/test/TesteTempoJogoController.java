7c14973b707db2eed00d4ddf431e1c31a79e993b
Vinicius <vininiciush@gmail.com>
2021-01-12 16:23:12 -0300

    Adicionando testes para classe TempoJogoController

--- src/test/java/test/TesteTempoJogoController.java
+++ src/test/java/test/TesteTempoJogoController.java
@@ -1,75 +1,89 @@
 package test;
 
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.compassouol.controller.TempoJogoController;
 import com.compassouol.controller.form.AddTempoJogoForm;
+import com.compassouol.dao.BibliotecaDao;
 import com.compassouol.dao.JogoDao;
 import com.compassouol.exceptions.DataInicioMaiorQueDataFimException;
 import com.compassouol.exceptions.JogosEmMesmoHorarioException;
 import com.compassouol.model.Jogo;
-@RunWith( SpringRunner.class )
-@SpringBootTest
+
+@SpringBootTest(classes = com.compassouol.Starter.class)
 class TesteTempoJogoController {
 
-	
+	@Autowired
+	TempoJogoController tempoJogoController;
+
 	@Test
 	void testAddTempoJogo() {
-		TempoJogoController tempoJogoController = new TempoJogoController();
 		JogoDao jogoDao = new JogoDao();
-		jogoDao.salva(new Jogo(70, "Nome Jogo","Desenvolvedora", "Distribuidora","26/07/1998", "Categoria", 100.0, "Descrição"));
-		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70,LocalDateTime.now(),LocalDateTime.now().plusHours(1));
+		jogoDao.salva(new Jogo(70, "Nome Jogo", "Desenvolvedora", "Distribuidora", "26/07/1998", "Categoria", 100.0,
+				"Descrição"));
+		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70, LocalDateTime.now(),
+				LocalDateTime.now().plusHours(1));
 		tempoJogoController.AddTempoJogoByDate(TempoJogoForm);
-		
-		
-		assertEquals(jogoDao.buscaPorId(70).getTempoJogado().size(), 1);
+
+		assertEquals(1, jogoDao.buscaPorId(70).getTempoJogado().size());
 	}
-	
+
 	@Test
 	void testAddTempoJogoDataFinalMaiorQueDataInicial() {
-		TempoJogoController tempoJogoController = new TempoJogoController();
 		JogoDao jogoDao = new JogoDao();
-		jogoDao.salva(new Jogo(70, "Nome Jogo","Desenvolvedora", "Distribuidora","26/07/1998", "Categoria", 100.0, "Descrição"));
-		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70,LocalDateTime.now().plusHours(1),LocalDateTime.now());
+		jogoDao.salva(new Jogo(70, "Nome Jogo", "Desenvolvedora", "Distribuidora", "26/07/1998", "Categoria", 100.0,
+				"Descrição"));
+		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70, LocalDateTime.now().plusHours(1),
+				LocalDateTime.now());
 		try {
 			tempoJogoController.AddTempoJogoByDate(TempoJogoForm);
 			fail();
-		}catch (DataInicioMaiorQueDataFimException e) {}
+		} catch (DataInicioMaiorQueDataFimException e) {
+		}
 	}
-	
+
 	@Test
 	void testAddTempoJogoDataConflitante() {
-		TempoJogoController tempoJogoController = new TempoJogoController();
 		JogoDao jogoDao = new JogoDao();
-		jogoDao.salva(new Jogo(70, "Nome Jogo","Desenvolvedora", "Distribuidora","26/07/1998", "Categoria", 100.0, "Descrição"));
-		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70,LocalDateTime.now(),LocalDateTime.now().plusHours(2));
-		AddTempoJogoForm TempoJogoForm2 = new AddTempoJogoForm(70,LocalDateTime.now().plusHours(1),LocalDateTime.now().plusHours(2));
+		jogoDao.salva(new Jogo(70, "Nome Jogo", "Desenvolvedora", "Distribuidora", "26/07/1998", "Categoria", 100.0,
+				"Descrição"));
+		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70, LocalDateTime.now(),
+				LocalDateTime.now().plusHours(2));
+		AddTempoJogoForm TempoJogoForm2 = new AddTempoJogoForm(70, LocalDateTime.now().plusHours(1),
+				LocalDateTime.now().plusHours(2));
 		tempoJogoController.AddTempoJogoByDate(TempoJogoForm);
-		
+
 		try {
 			tempoJogoController.AddTempoJogoByDate(TempoJogoForm2);
 			fail();
-		}catch (JogosEmMesmoHorarioException e) {}
-		
+		} catch (JogosEmMesmoHorarioException e) {
+		}
+
 	}
-	
-	
+
 	@Test
 	void testAddTempoJogoJogoNaoExiste() {
-		TempoJogoController tempoJogoController = new TempoJogoController();
-		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70,LocalDateTime.now(),LocalDateTime.now().plusHours(1));
-		
-		assertEquals(tempoJogoController.AddTempoJogoByDate(TempoJogoForm).getStatusCode().compareTo(HttpStatus.NOT_FOUND),0); 
-		
+		AddTempoJogoForm TempoJogoForm = new AddTempoJogoForm(70, LocalDateTime.now(),
+				LocalDateTime.now().plusHours(1));
+
+		assertEquals(
+				tempoJogoController.AddTempoJogoByDate(TempoJogoForm).getStatusCode().compareTo(HttpStatus.NOT_FOUND),
+				0);
+
+	}
+	
+	@AfterEach
+	void limpaBiblioteca() {
+		BibliotecaDao.biblioteca.getJogos().clear();
 	}
 
 }
