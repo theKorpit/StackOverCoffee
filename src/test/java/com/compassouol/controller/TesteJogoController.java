50ceb5d8e9785c114cbb3e906bddf4151ded12ba
Vinicius Michel Schunke <vininiciush@gmail.com>
2021-02-03 14:14:14 -0300

    Corigindo teste e alterando para usar mock

--- src/test/java/com/compassouol/controller/TesteJogoController.java
+++ src/test/java/com/compassouol/controller/TesteJogoController.java
@@ -1,78 +1,125 @@
 package com.compassouol.controller;
 
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.compassouol.controller.JogoController;
-import com.compassouol.dao.BibliotecaDao;
+import com.compassouol.dto.entrada.JogoDtoEntrada;
 import com.compassouol.exceptions.JogoInvalidoException;
+import com.compassouol.model.Jogo;
+import com.compassouol.services.JogoService;
+import com.compassouol.services.TempoJogoService;
 
-@SpringBootTest(classes = com.compassouol.Starter.class)
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+@WebMvcTest
 class TesteJogoController {
 
 	@Autowired
 	JogoController jogoController;
-
-	@Test
-	void testAdicionaJogoIdValido() throws IOException, ParseException {
-		jogoController.adicionaJogo("70");
-		assertTrue(BibliotecaDao.biblioteca.getJogos().size() > 0);
+	
+	@MockBean
+	JogoService jogoService;
+	
+	@MockBean
+	TempoJogoService tempojogoService;
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	private Jogo jogoCs = new Jogo(730, "Counter-Strike: Global Offensive", "Valve,Hidden Path Entertainment", "Valve", "21 Aug 2012", "Multi-player,Action,Free to Play", 0.0, null);
+	
+	@BeforeEach
+	public void setup() {
+		RestAssuredMockMvc.standaloneSetup(this.jogoController);
 	}
 
-	@Test
-	void testAdicionaJogoNomeValido() throws IOException, ParseException {
-		jogoController.adicionaJogo("Half-life");
-		assertTrue(BibliotecaDao.biblioteca.getJogos().size() > 0);
-	}
 
 	@Test
-	void testAdicionaJogoIdInvalido() throws IOException, ParseException {
-		try {
-			jogoController.adicionaJogo("1");
-		} catch (JogoInvalidoException e) {
-		}
-
-		assertTrue(BibliotecaDao.biblioteca.getJogos().size() == 0);
-	}
-
-	@Test
-	void testAdicionaJogoNomeInvalido() throws IOException, ParseException {
-		try {
-			jogoController.adicionaJogo("saskjendw");
-		} catch (JogoInvalidoException e) {
-		}
-
-		assertTrue(BibliotecaDao.biblioteca.getJogos().size() == 0);
-	}
-
-	@Test
-	void testBuscaJogoIdValido() throws IOException, ParseException {
-		jogoController.adicionaJogo("70");
-		assertDoesNotThrow(() -> {
-			jogoController.buscaJogoPorId(70);
-		});
-
+	void testAdicionaJogoIdValido() throws Exception {
+		
+		JogoDtoEntrada jogoDtoEntrada = new JogoDtoEntrada(730, null);
+		
+		when(this.jogoService.adicionaJogoBiblioteca(jogoDtoEntrada)).thenReturn(jogoCs);
+		
+		mvc.perform(post("/jogo").content("{\n"
+				+ "    \"idSteam\":\"730\",\n"
+				+ "    \"nomeJogo\":\"\"\n"
+				+ "}")
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isOk());
 	}
 
 	@Test
-	void testBuscaJogoIdInvalido() throws IOException, ParseException {
-		jogoController.adicionaJogo("70");
-		assertThrows(JogoInvalidoException.class, () -> {
-			jogoController.buscaJogoPorId(1);
-		});
+	void testAdicionaJogoNomeValido() throws Exception {
+		JogoDtoEntrada jogoDtoEntrada = new JogoDtoEntrada(null, "Counter-Strike: Global Offensive");
+		
+		when(this.jogoService.adicionaJogoBiblioteca(jogoDtoEntrada)).thenReturn(jogoCs);
+		
+		mvc.perform(post("/jogo").content("{\n"
+				+ "    \"idSteam\":\"\",\n"
+				+ "    \"nomeJogo\":\"Counter-Strike: Global Offensive\"\n"
+				+ "}")
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isOk());
 	}
 
-	@AfterEach
-	void limpaBiblioteca() {
-		BibliotecaDao.biblioteca.getJogos().clear();
+	@Test
+	void testBuscaTodos() throws Exception {
+		
+		List<Jogo> jogos = new ArrayList<Jogo>();
+		jogos.add(jogoCs);
+		
+		when(this.jogoService.retornaJogos()).thenReturn(jogos);
+		
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get("/jogo")
+		.then()
+			.statusCode(HttpStatus.OK.value());
 	}
+	
+	@Test
+	void testBuscaJogoPorIdJogoExisteNoBancoDeDados() {
+		
+		when(this.jogoService.retornaJogoPorId(730)).thenReturn(jogoCs);
+		
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get("/jogo/{id}",730)
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	void testBuscaJogoPorIdJogoNaoExisteNoBancoDeDados() throws Exception {
+		
+		when(this.jogoService.retornaJogoPorId(730)).thenReturn(null);
+		
+		mvc.perform(get("/jogo/{id}", 730)
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isNotFound())
+			      .andExpect(result -> assertTrue(result.getResolvedException() instanceof JogoInvalidoException))
+			      .andExpect(result -> assertEquals("Nao foi localizado nenhum jogo com os dados informados no campo de busca acima!", result.getResolvedException().getMessage()));
+	}
+	
 }
--- src/test/java/testService/TesteJogoService.java
+++ src/test/java/testService/TesteJogoService.java
@@ -1,59 +1,100 @@
 package testService;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.compassouol.dao.BibliotecaDao;
-import com.compassouol.exceptions.JogoInvalidoException;
+import com.compassouol.dto.entrada.JogoDtoEntrada;
+import com.compassouol.exceptions.JogoDuplicadoException;
+import com.compassouol.model.Jogo;
+import com.compassouol.repository.JogoRepository;
 import com.compassouol.services.JogoService;
 
+@SpringBootTest(classes = com.compassouol.Starter.class)
 class TesteJogoService {
 	
-	JogoService jogoService = new JogoService();
+	@Autowired
+	JogoService jogoService;
+	
+	@MockBean
+	JogoRepository jogoRepository;
+	
+	private JogoDtoEntrada jogoDtoEntrada;
+	
+	private Jogo jogoCs = new Jogo(730, "Counter-Strike: Global Offensive", "Valve,Hidden Path Entertainment", "Valve", "21 Aug 2012", "Multi-player,Action,Free to Play", 0.0, null);
 	
 	@Test
-	void testVerificaIdValido() {
-		assertTrue(jogoService.verificaId("70"));
+	void testAdicionaJogoNovoNaBibliotecaPorIdQuandoJogoNaoEstaNoBancoDeDados() throws IOException, ParseException {
+		
+		when(this.jogoRepository.findById(730)).thenReturn(Optional.empty());
+		
+		jogoDtoEntrada = new JogoDtoEntrada(730, null);
+		
+		Jogo jogo = jogoService.adicionaJogoBiblioteca(jogoDtoEntrada);
+		
+		assertEquals(730, jogo.getAppIdSteam());
+		assertEquals("Counter-Strike: Global Offensive", jogo.getNomeJogo());
 	}
+	
+	@Test
+	void testAdicionaJogoNovoNaBibliotecaPorIdQuandoJogoEstaNoBancoDeDados() throws IOException, ParseException {
+		
+		when(this.jogoRepository.findById(730)).thenReturn(Optional.of(jogoCs));
+		
+		jogoDtoEntrada = new JogoDtoEntrada(730, null);
+		
+		assertThrows(JogoDuplicadoException.class, () ->  jogoService.adicionaJogoBiblioteca(jogoDtoEntrada));
+		
+	}
+	
+	@Test
+	void testAdicionaJogoNovoNaBibliotecaPorNomeQuandoJogoNaoEstaNoBancoDeDados() throws IOException, ParseException {
+		
+		when(this.jogoRepository.findByNomeJogo("Half-life")).thenReturn(null);
+		
+		JogoDtoEntrada jogoDtoEntrada = new JogoDtoEntrada(null, "Half-life");
+		
+		Jogo jogo = jogoService.adicionaJogoBiblioteca(jogoDtoEntrada);
+		
+		assertEquals(70, jogo.getAppIdSteam());
+		assertEquals("Half-life", jogo.getNomeJogo());
+	}
+	
+	@Test
+	void testAdicionaJogoNovoNaBibliotecaPorNomeQuandoJogoEstaNoBancoDeDados() throws IOException, ParseException {
+		
+		when(this.jogoRepository.findByNomeJogo("Counter-Strike: Global Offensive")).thenReturn(jogoCs);
+		
+		jogoDtoEntrada = new JogoDtoEntrada(null, "Counter-Strike: Global Offensive");
+		
+		assertThrows(JogoDuplicadoException.class, () ->  jogoService.adicionaJogoBiblioteca(jogoDtoEntrada));
+	}
+	
 
 	@Test
-	void testVerificaIdInvalido() {
-		assertFalse(jogoService.verificaId("Half-life"));
+	void testRetornaJogoPorIdJogoExistenteNoBanco() throws IOException, ParseException {
+		
+		when(this.jogoRepository.findById(730)).thenReturn(Optional.of(jogoCs));
+		
+		assertTrue(jogoService.retornaJogoPorId(730) != null);
 	}
 	
 	@Test
-	void testAdicionaJogoNovoNaBibliotecaPorId() throws IOException, ParseException {
-		assertTrue(jogoService.adicionaJogoBiblioteca("730"));
+	void testRetornaJogoPorIdJogoInexistententeNoBanco() throws IOException, ParseException {
+		
+		when(this.jogoRepository.findById(730)).thenReturn(Optional.empty());
+		
+		assertTrue(jogoService.retornaJogoPorId(730) == null);
 	}
 	
-	@Test
-	void testAdicionaJogoNovoNaBibliotecaPorNome() throws IOException, ParseException {
-		assertTrue(jogoService.adicionaJogoBiblioteca("Half-life"));
-	}
-	
-	@Test
-	void testNaoiAdicionaJogoExistenteNaBiblioteca() throws IOException, ParseException {
-		jogoService.adicionaJogoBiblioteca("Half-life");
-		assertThrows(JogoInvalidoException.class, () -> {
-			jogoService.adicionaJogoBiblioteca("70");
-		});
-	}
-	
-	@Test
-	void testRetornaJogoPorId() throws IOException, ParseException {
-		jogoService.adicionaJogoBiblioteca("Half-life");
-		assertTrue(jogoService.retornaJogoPorId(70) != null);
-	}
-	
-	@AfterEach
-	void limpaBiblioteca() {
-		BibliotecaDao.biblioteca.getJogos().clear();
-	}
 }
